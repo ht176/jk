f0551dd404a963ec7f230b187388bfd529733d91
何东 <9002644+he__dong@user.noreply.gitee.com>
2021-09-05 23:55:16 +0800

    feat 状态机处理字符串

---
+++ 第一周浏览器工作原理/2/match.js
@@ -0,0 +1,13 @@
+function match (string) {
+  let foundA = false
+  for (let c of string) {
+    if (c === 'a')
+      foundA = true
+    else if (foundA && c === 'b')
+      return true
+    else
+      foundA = false
+  }
+  return false
+}
+console.log(match('I acabm groot'));
